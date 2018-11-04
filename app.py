015179b8880f8640457e3e57796d8c456eaeb4e1
sushen <sushenbiswasaga@gmail.com>
2018-11-04 13:27:57 +0600

    Commit Procfile and requirements.txt

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app =Flask(__name__)
 
 @app.route("/")
 def index():
-    return "This is my Matrivume Application"
+    return "This is my Matrivume Application Pipeline"
 
 if __name__ == "__main__":
     app.run(use_reloader=True)
