92abacfa9bd3e24e5700f6f499cf4bbfa6e30fb4
sushen <sushenbiswasaga@gmail.com>
2018-11-04 20:37:59 +0600

    Template File add

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app =Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    return "This is my Matrivume for holelife"
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(use_reloader=True)
 
+@app.route('/Privacy-Policy')
+def PrivacyPolicy():
+    return render_template("Privacy-Policy.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
